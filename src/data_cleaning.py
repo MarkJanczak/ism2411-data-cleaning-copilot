ce3e1a550d36a8a7b15d24799a7a533d283ab182
MarkJanczak <dar359@cornell.edu>
2026-04-26 23:16:07 -0400

    Simply copy and pasted the code given in the text file and inserted comments for Copilot to read above the methods.

---
+++ src/data_cleaning.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# method to read in sales_data_raw.csv from data/raw/.
+def load_data(file_path:str):
+        ...
+
+# method to make the column names lowercased and underscores in the .csv.
+def clean_column_names(df):
+        ...
+
+# method to deal with empty cells in the csv
+def handle_missing_values(df):
+        ...
+
+# method to remove invalid rows in the csv.
+def remove_invalid_rows(df):
+        ...
+
+
+
+if __name__ == "__main__":
+        raw_path = "data/raw/sales_data_raw.csv"
+        cleaned_path = "data/processed/sales_data_clean.csv"
+
+        df_raw = load_data(raw_path)
+        df_clean = clean_column_names(df_raw)
+        df_clean = handle_missing_values(df_clean)
+        df_clean = remove_invalid_rows(df_clean)
+        df_clean.to_csv(cleaned_path, index=False)
+        print("Cleaning complete. First few rows:")
+        print(df_clean.head())
