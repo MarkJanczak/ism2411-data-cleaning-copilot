e9efdc376dbcce823b4ab2e956ffa1cb24035f12
MarkJanczak <dar359@cornell.edu>
2026-04-26 23:31:56 -0400

    Added all of the comments for now. Purpose and Why

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,18 +1,28 @@
+"""
+        Purpose : cleans data/raw/sales_data_raw.csv by 
+                  removing invalid rows with negative values, 
+                  trimming whitespace and column names, and 
+                  handling missing values.
+"""
 import pandas as pd
 
 # method to read in sales_data_raw.csv from data/raw/.
+# Why : doing it to "clean" the .csv.
 def load_data(file_path:str):
         ...
 
 # method to make the column names lowercased and underscores in the .csv.
+# Why : To make the column names have the same style and look professional.
 def clean_column_names(df):
         ...
 
 # method to deal with empty cells in the csv
+# Why : we either remove the cell or fill it in with an empty value. For proper data collection.
 def handle_missing_values(df):
         ...
 
 # method to remove invalid rows in the csv.
+# Why : invalid rows spoil the data. rows that have negative quantity or price.
 def remove_invalid_rows(df):
         ...
 
